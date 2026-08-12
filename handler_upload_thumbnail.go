6f0e1292b832ddcd60ab88c1474854940a430ea7
Asad <aq8829@outlook.com>
2026-08-12 19:04:10 -0400

    added thumbnail uploading logic

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -9,10 +10,20 @@ import (
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
-	videoIDString := r.PathValue("videoID")
-	videoID, err := uuid.Parse(videoIDString)
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		respondWithError(w, http.StatusBadRequest, "Couldn't read thumbnail file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+	mediaData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't read thumbnail data", err)
 		return
 	}
 
@@ -22,16 +33,44 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	metaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't retrieve video data", err)
+		return
+	}
+
 	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
 		return
 	}
+	if userID != metaData.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Thumbnail does not belong to user", nil)
+		return
+	}
 
+	var videoThumbnail = thumbnail{
+		data:      mediaData,
+		mediaType: mediaType,
+	}
+	videoThumbnails[videoID] = videoThumbnail
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	metaData.ThumbnailURL = &url
 
-	// TODO: implement the upload here
-
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	err = cfg.db.UpdateVideo(metaData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video data", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, metaData)
 }
